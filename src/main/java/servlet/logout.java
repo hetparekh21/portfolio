23476e0cfdd10a26be411b20fbe0ccee54f0d895
Het <hetparekh21@gmail.com>
2022-10-01 18:38:03 +0530

    changes for cookie functioning

--- src/main/java/servlet/logout.java
+++ src/main/java/servlet/logout.java
@@ -31,7 +31,7 @@ public class logout extends HttpServlet {
 		System.out.println("logedout");
 		// removing cookies
 		Cookie c = new Cookie("user_id", "");
-		c.setPath("/");
+		c.setPath("/portfolyio/admin");
 		c.setMaxAge(0);
 		response.addCookie(c);
 		
